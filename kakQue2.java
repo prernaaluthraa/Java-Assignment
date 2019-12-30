c69d7a12fa626592fe2291d1a6f899a298d456d3
prernaaluthraa <58359868+prernaaluthraa@users.noreply.github.com>
2019-12-31 01:13:26 +0530

    Create kakQue2.java

---
+++ kakQue2.java
@@ -0,0 +1,49 @@
+package questions;
+import java.util.*;
+public class Ques3 
+{
+	  static int evaluatePostfix(String exp) 
+	    { 
+
+	        Stack<Integer> stack=new Stack<>(); 
+
+	        for(int i=0;i<exp.length();i++) 
+	        { 
+	            char c=exp.charAt(i); 
+	            if(Character.isDigit(c)) 
+	            stack.push(c - '0'); 
+	            else
+	            { 
+	                int val1 = stack.pop(); 
+	                int val2 = stack.pop(); 
+	                  
+	                switch(c) 
+	                { 
+	                    case '+': 
+	                    stack.push(val2+val1); 
+	                    break; 
+	                      
+	                    case '-': 
+	                    stack.push(val2- val1); 
+	                    break; 
+	                      
+	                    case '/': 
+	                    stack.push(val2/val1); 
+	                    break; 
+	                      
+	                    case '*': 
+	                    stack.push(val2*val1); 
+	                    break; 
+	              } 
+	            } 
+	        } 
+	        return stack.pop();     
+	    } 
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String exp=sc.next(); 
+	    System.out.println("postfix evaluation: "+evaluatePostfix(exp)); 
+	}
+
+}
