3f05343ccd15ffbe14ddf37426f0503a66e8f070
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2020-01-22 16:04:28 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,25 @@
+package one;
+
+public class Cube {
+	int len;
+	int bre;
+	int width;
+	
+	public int getCubeVol() {
+		return(len*bre*width);
+		
+	}
+	Cube() {
+		len=10;
+		bre=20;
+		width=40;
+	}
+	Cube (int l,int b, int w) {
+		len = l;
+		bre = b;
+		width = w;
+		
+		
+	}
+	}
+
