8e08f194767d163096e1e8e89b4fa710ead84df0
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2020-01-22 16:03:20 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,26 @@
+package one;
+
+public class student {
+	int Id;
+	String name;
+	int age;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int Id) {
+		this.Id = Id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
