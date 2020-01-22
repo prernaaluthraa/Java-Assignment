e2901f17abc14b2f6337e8e7b9036848bcb0b826
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2020-01-22 16:04:05 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,138 @@
+package one;
+
+import java.util.Scanner;
+
+public class MyClass {
+	public static void main(String[] args) {
+		/*Scanner scan= new Scanner(System.in);
+		/*
+		 System.out.println("enter any number");
+		 int no=scan.nextInt();
+		 System.out.println("the entered number is");
+		 System.out.print(no);
+		 
+		 Scanner scan1= new Scanner(System.in);
+		 System.out.println("enter any number");
+		 double d=scan1.nextDouble();
+		 System.out.println("the entered number is");
+		 System.out.print(d);
+		*/
+		/*int x , y, answer;
+		x=20;
+		y=30;
+		answer=x+y;
+		System.out.println("Answer is" + answer);
+		
+		// %(modulus)- returns the remainder
+		  */
+	
+		// && and , || or
+		/*
+		int a=5;
+		while (a < 5)
+		{
+			System.out.println(a);
+			a++;
+		
+			
+		}
+		
+		*/
+		/*
+      int arr[]= {1,2,3,4,5};
+      int index=0;
+      
+      while(index<5)
+      {
+    	  System.out.println(arr[index]);
+    	  index++;
+      }
+		*/
+		/*
+		int arr[]= {1,2,3,4,5};
+		
+		for (int element : arr)
+		{
+			System.out.println(arr[index]);
+		}
+		
+		
+		*/
+		/*
+		String s="hey " + "prerna";
+		int len=s.length();
+		String leninUpperCase=s.toUpperCase();
+		System.out.println(s.indexOf('r'));
+	
+		
+		*/
+		//void-return type, no return value
+		
+	/*
+		int sum=Add(2,3);
+		int result=sum*10;
+		System.out.println(result);
+		
+		
+	}
+	public static  void myFirstMethod(String name) {
+		System.out.println("helloooo" + name);
+		
+	}
+	public static int Add(int a,int b)
+	{
+		//System.out.println(a+b);
+		return(a+b);
+		
+		*/
+		/*student mark = new student();//mark-object or instance
+		mark.id = 1;
+		mark.name="Mark";
+		mark.age=15;
+		System.out.println(mark.name+ mark.age);
+		
+		student prerna = new student();//mark-object or instance
+		prerna.id = 1;
+		prerna.name="prerna";
+		prerna.age=15;
+		System.out.println(prerna.name+ prerna.age);
+	*/
+		/*
+		student prerna = new student();
+		prerna.setId(1);
+		prerna.setName("prerna");
+		prerna.setAge(15);
+System.out.println(prerna.age);
+*/
+		/*
+		Cube cube1 = new Cube();
+		//contructor never returns any value. name exactly same as the class name.
+		System.out.println(cube1.getCubeVol());
+		
+		Cube cube2 = new Cube(20,20,10);
+		//contructor never returns any value. name exactly same as the class name.
+		//contructor only declare any value.
+		System.out.println(cube1.getCubeVol());
+		*/
+/*	System.out.println(Add(2,5));	
+	System.out.println(Add(2.56,5.25));
+	}
+	public static int Add(int a, int b) {
+		return(a+b);
+		
+	}
+	public static double Add(double a, double b) {
+		return(a+b);
+}
+	public static String Add(String a, String b) {
+		return(a+b);*/
+		// // // static-  returns the static member belongs to the class
+// static member belongs to the class intead of the specific instance.
+		//cannot access as hello.dosomething
+	/*	Hello hello = new Hello();
+		Hello.dosomething("hi youtube");//to  call static method
+		hello.dosomething("hi prerna");//to call the unstatic method
+*/		
+		
+	}
+}
