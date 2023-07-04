4e479970ea952b3d07c4418c5f376f4dfff281b5
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-07-04 07:13:33 -0400

    Update ArrayAltPrint.java

--- ArrayAltPrint.java
+++ ArrayAltPrint.java
@@ -1,48 +1,58 @@
-import os
-import time
-import csv
+import smtplib,ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
-# ... (existing code)
-
-# Get disk storage before running the script
-total_before = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_blocks
-free_before = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_bfree
-
-# ... (existing code)
-
-output_filename = "/application/RPA/COMMON/CleanupFiles/LOGS/output_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
-with open(output_filename, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["Action", "File Path", "Modification Date"])
-
-    # ... (existing code)
-
-    # Get disk storage after running the script
-    total_after = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_blocks
-    free_after = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_bfree
-
-    # Calculate used space
-    used_before = total_before - free_before
-    used_after = total_after - free_after
-
-    # Convert sizes to human-readable format
-    total_before_str = f"{total_before / (1024 ** 3):.2f} GB"
-    used_before_str = f"{used_before / (1024 ** 3):.2f} GB"
-    free_before_str = f"{free_before / (1024 ** 3):.2f} GB"
-
-    total_after_str = f"{total_after / (1024 ** 3):.2f} GB"
-    used_after_str = f"{used_after / (1024 ** 3):.2f} GB"
-    free_after_str = f"{free_after / (1024 ** 3):.2f} GB"
-
-    # Write disk storage information in the CSV file
-    csvwriter.writerow(["Disk Storage Before Script Execution"])
-    csvwriter.writerow(["Total", total_before_str])
-    csvwriter.writerow(["Used", used_before_str])
-    csvwriter.writerow(["Free", free_before_str])
-    csvwriter.writerow([])  # Empty row for separation
-    csvwriter.writerow(["Disk Storage After Script Execution"])
-    csvwriter.writerow(["Total", total_after_str])
-    csvwriter.writerow(["Used", used_after_str])
-    csvwriter.writerow(["Free", free_after_str])
-
-    # ... (existing code)
+def sendOutlookMail(obj):
+    port = int(obj.config['ExchangePort'])  # For SSL
+    SERVER = obj.config['ExchangeServer']
+    msg=MIMEMultipart('alternative')
+    # asset=obj.get_password(obj.config['asset_email'])
+    # frm=asset[0]
+    # msg=MIMEMultipart('alternative')
+    asset=obj.get_password(obj.config['asset_email'])
+    frm=asset[0]
+    #frm=r'svcs_rpa_prod_bot9@mortgagefamily.com'
+    # pwd= "hvtaJ$m7"
+    # frm=obj.config['mailId']
+    disclamerfile= open(obj.config['SIG'],'r')
+    disclamer=disclamerfile.read()
+    disclamerfile.close()
+    to=obj.config['ExchEmailTo'].split(";")
+    cc=obj.config['ExchEmailCc'].split(";")
+    msg['From']=frm
+    msg['To']=','.join(to)
+    msg['Cc']=','.join(cc)
+    BODY=''
+    to+=cc
+    if obj.SystemException:
+        msg['Subject']=obj.config['BU']+"-"+obj.config['PROCESSNAME']+" "+obj.SystemException+str(obj.metrics.StartTime)[:-7].replace("-","/")
+    elif obj.BusinessRuleException:
+        msg['Subject']=obj.config['BU']+"-"+obj.config['PROCESSNAME']+" Failed!"+str(obj.metrics.StartTime)[:-7].replace("-","/")
+        BODY+=obj.config['ExchEmailBodyBRE']
+    elif obj.TransactionData is None:
+        msg['Subject']=obj.config['BU']+"-"+obj.config['PROCESSNAME']+" Input File is missing"+str(obj.metrics.StartTime)[:-7].replace("-","/")
+        BODY+=obj.config['ExchEmailBodyNOFILE']
+    elif len(obj.TransactionData)==0:
+        msg['Subject']=obj.config['BU']+"-"+obj.config['PROCESSNAME']+" No Valid Data found in the Input File"+str(obj.metrics.StartTime)[:-7].replace("-","/")
+        BODY+=obj.config['ExchEmailBodyNODATA']
+    elif obj.metrics.JobStatus!=1:
+        msg['Subject']=obj.config['BU']+"-"+obj.config['PROCESSNAME']+" "+str(obj.metrics.StartTime)[:-7].replace("-","/")
+        BODY+=obj.config['ExchEmailBodySuccess']
+        STATS=obj.config['ExchEmailBodyReport'].replace('XXXXX',str(obj.totalLoans)).replace('YYYYY',str(obj.ProcessedLoans)).replace('ZZZZZ',str(obj.metrics.SuccessCount))
+        RATIO=obj.config['ExchEmailBodyTime'].replace('AAAAA',str(obj.metrics.StartTime)[:-7].replace("-","/")).replace('BBBBB',str(obj.metrics.EndTime)[:-7].replace("-","/"))
+        BODY+=STATS+RATIO
+        with open(obj.outputfile,'rb') as atch:
+            part=MIMEApplication(atch.read(),Name=obj.outputfile)
+        part['Content-Disposition']='attachment;filename="{}"'.format(obj.outputfile.split('/')[-1])
+        msg.attach(part)
+    BODY+=disclamer
+    msg.attach(MIMEText(BODY,'plain'))
+    msg.attach(MIMEText(BODY,'html'))
+    context = ssl.create_default_context()
+    server=smtplib.SMTP(SERVER,port)
+    server.starttls(context=context)
+    #server.login(frm,asset[1])
+    # server.login(frm,obj.config['mailPass'])
+    server.sendmail(frm,to,msg.as_string())
+    server.quit()
