3880093192398ebd64356e54afea2a282986708f
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-07-04 16:14:13 +0530

    Update AddLink.java

--- AddLink.java
+++ AddLink.java
@@ -2,27 +2,32 @@ import os
 import time
 import csv
 
-# ... (existing code)
-
-# Get disk storage before running the script
-total_before = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_blocks
-free_before = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_bfree
-
-# Convert sizes to human-readable format
-total_before_str = f"{total_before / (1024 ** 3):.2f} GB"
-free_before_str = f"{free_before / (1024 ** 3):.2f} GB"
-
-# Print disk storage information before running the script
-print("Disk Storage Before Script Execution:")
-print(f"Total: {total_before_str}")
-print(f"Free: {free_before_str}")
-
-# ... (existing code)
+rpa_path = "/application/RPA"
+folders_to_check = ["LOGS", "SCREENSHOTS", "ARCHIVE", "OUT"]
+three_days_ago = time.time() - (180 * 24 * 60 * 60)
+log_count = 0
+screenshot_count = 0
+others_count = 0
+size = 0
 
 output_filename = "/application/RPA/COMMON/CleanupFiles/LOGS/output_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
 with open(output_filename, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(["Action", "File Path", "Modification Date"])
+
+    # Get disk storage before running the script
+    total_before = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_blocks
+    free_before = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_bfree
+
+    # Convert sizes to human-readable format
+    total_before_str = f"{total_before / (1024 ** 3):.2f} GB"
+    free_before_str = f"{free_before / (1024 ** 3):.2f} GB"
+
+    # Print disk storage information before running the script
+    csvwriter.writerow(["Disk Storage Before Script Execution"])
+    csvwriter.writerow(["Total", total_before_str])
+    csvwriter.writerow(["Free", free_before_str])
+    csvwriter.writerow([])  # Empty row for separation
 
     for root, dirs, files in os.walk(rpa_path):
         for folder_name in folders_to_check:
@@ -40,12 +45,9 @@ with open(output_filename, 'w', newline='') as csvfile:
                                 mod_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(mod_time))
                                 csvwriter.writerow(["File path:", file_path, mod_date])
                                 log_count += 1
-
                                 #os.remove(file_path)
                     else:
                         pass 
-
-                            
 
                 elif folder_name == "SCREENSHOTS":
                     print(folder_path)
@@ -63,7 +65,7 @@ with open(output_filename, 'w', newline='') as csvfile:
                             
                             else:
                                 pass
-           
+
                 else:
                     for filename in os.listdir(folder_path):
                         file_path = os.path.join(folder_path, filename)
@@ -72,24 +74,24 @@ with open(output_filename, 'w', newline='') as csvfile:
                             mod_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(mod_time))
                             csvwriter.writerow(["File path", file_path, mod_date])
                             others_count += 1
-
                             #os.remove(file_path) 
 
-# Get disk storage after running the script
-total_after = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_blocks
-free_after = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_bfree
+    # Get disk storage after running the script
+    total_after = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_blocks
+    free_after = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_bfree
 
-# Calculate used space
-used_before = total_before - free_before
-used_after = total_after - free_after
+    # Calculate used space
+    used_before = total_before - free_before
+    used_after = total_after - free_after
 
-# Convert sizes to human-readable format
-total_after_str = f"{total_after / (1024 ** 3):.2f} GB"
-used_after_str = f"{used_after / (1024 ** 3):.2f} GB"
-free_after_str = f"{free_after / (1024 ** 3):.2f} GB"
+    # Convert sizes to human-readable format
+    total_after_str = f"{total_after / (1024 ** 3):.2f} GB"
+    used_after_str = f"{used_after / (1024 ** 3):.2f} GB"
+    free_after_str = f"{free_after / (1024 ** 3):.2f} GB"
 
-# Print disk storage information after running the script
-print("Disk Storage After Script Execution:")
-print(f"Total: {total_after_str}")
-print(f"Used: {used_after_str}")
-print(f"Free: {free_after_str}")
+    # Print disk storage information after running the script
+    csvwriter.writerow([])  # Empty row for separation
+    csvwriter.writerow(["Disk Storage After Script Execution"])
+    csvwriter.writerow(["Total", total_after_str])
+    csvwriter.writerow(["Used", used_after_str])
+    csvwriter.writerow(["Free", free_after_str])
