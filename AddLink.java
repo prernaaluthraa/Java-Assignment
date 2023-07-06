a49468d756bd824629ca4b006b06d97db8e289a4
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-07-07 05:05:51 +0530

    Update AddLink.java

--- AddLink.java
+++ AddLink.java
@@ -1,15 +1,10 @@
-    try:
-        time.sleep(2)
-        #in_interestrate = '0.0125'
-        interestrate_element = driver.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[1]/div/div[2]/div/div[2]/div[2]/div[1]/div/div/div/div/table/tbody/tr[1]/td[2]/div/div/div/span/input')
-        print("interestrate field to be updated",obj.interest_rate_reduction_increments)
-        obj.printlog.info(f"interestrate field to be updated",obj.interest_rate_reduction_increments)
+    def __init__(self, ConfigPath):
+        self.config = self.load_config(ConfigPath)
+        self.SystemException = None
 
-        interestrate_element.send_keys(obj.interest_rate_reduction_increments)
-        print("interestrate_element updated.", obj.interest_rate_reduction_increments)    
-        obj.printlog.info(f"interestrate_element updated." , obj.interest_rate_reduction_increments)
-    except (TimeoutException, StaleElementReferenceException) as e:
-        traceback.print_exc()
-        print(f"error:{e}")     
-    except Exception as ex:
-        raise Exception(f"error:", str(ex))
+    def load_config(self, ConfigPath):
+        df = pd.read_csv(ConfigPath)
+        config = {}
+        for _, row in df.iterrows():
+            config[row['NAME']] = row['VALUE']
+        return config
