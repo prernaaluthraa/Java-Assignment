dcdce0b601df6225259a7cbbc4abb5238745ba11
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-07-06 05:07:44 -0400

    Update AddLink.java

--- AddLink.java
+++ AddLink.java
@@ -1,4 +1,15 @@
-1. I've outperformed the goals that were assigned to me.
-2. Taken up new development activities and production issues along with handling L1 activities regularly.
-3. Have always been flexible with my working hours to accomodate the team.
-4. Able to deliver exceptional results when it comes to providing the operational support for the processes developed for any business unit.
+    try:
+        time.sleep(2)
+        #in_interestrate = '0.0125'
+        interestrate_element = driver.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[1]/div/div[2]/div/div[2]/div[2]/div[1]/div/div/div/div/table/tbody/tr[1]/td[2]/div/div/div/span/input')
+        print("interestrate field to be updated",obj.interest_rate_reduction_increments)
+        obj.printlog.info(f"interestrate field to be updated",obj.interest_rate_reduction_increments)
+
+        interestrate_element.send_keys(obj.interest_rate_reduction_increments)
+        print("interestrate_element updated.", obj.interest_rate_reduction_increments)    
+        obj.printlog.info(f"interestrate_element updated." , obj.interest_rate_reduction_increments)
+    except (TimeoutException, StaleElementReferenceException) as e:
+        traceback.print_exc()
+        print(f"error:{e}")     
+    except Exception as ex:
+        raise Exception(f"error:", str(ex))
