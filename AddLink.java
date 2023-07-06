4c88eb072375096e8c5d9ffbd177fce02ed02f16
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-07-06 19:37:12 -0400

    Update AddLink.java

--- AddLink.java
+++ AddLink.java
@@ -1,10 +1,5 @@
-    def __init__(self, ConfigPath):
-        self.config = self.load_config(ConfigPath)
-        self.SystemException = None
-
-    def load_config(self, ConfigPath):
-        df = pd.read_csv(ConfigPath)
-        config = {}
-        for _, row in df.iterrows():
-            config[row['NAME']] = row['VALUE']
-        return config
+File "/application/RPA/COMMON/CleanupFiles/UpdatedCleanup.py", line 156, in sendOutlookMail
+    asset=obj.get_password(obj.config['asset_email'])
+  File "/application/RPA/COMMON/CleanupFiles/UpdatedCleanup.py", line 139, in get_password
+    cursor=self.asset_connection.cursor()
+AttributeError: 'Obj' object has no attribute 'asset_connection'
